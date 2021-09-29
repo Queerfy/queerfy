3e3f750f470f064fde5ea77f75ae8d516ca75b15
nicolasconde <nicolasconde196@gmail.com>
2021-09-29 17:08:08 -0300

    .

--- Front-end/src/pages/Account/Notification/styles.ts
+++ Front-end/src/pages/Account/Notification/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const ContainerRow = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
 `;
