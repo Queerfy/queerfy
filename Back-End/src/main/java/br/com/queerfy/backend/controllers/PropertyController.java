5c43064ac74c3ed268415ae8787faf32cd492cfe
rafaelholland <rafael.holland@bandtec.com.br>
2021-10-07 18:48:45 -0300

    ädded new route all properties

--- Back-End/src/main/java/br/com/queerfy/backend/controllers/PropertyController.java
+++ Back-End/src/main/java/br/com/queerfy/backend/controllers/PropertyController.java
@@ -6,12 +6,12 @@ import br.com.queerfy.backend.exceptions.UserAlreadyExistsException;
 import br.com.queerfy.backend.exceptions.UserNotFoundException;
 import br.com.queerfy.backend.services.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.stylesheets.LinkStyle;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("properties")
@@ -23,5 +23,10 @@ public class PropertyController {
     @PostMapping
     public PropertyDTO create(@RequestBody @Valid PropertyDTO propertyDTO) throws UserAlreadyExistsException, UserNotFoundException {
         return service.create(propertyDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PropertyDTO>> getAllProperties(){
+        return ResponseEntity.status(200).body(service.getAllProperties());
     }
 }
