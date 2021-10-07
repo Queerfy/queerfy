b38ff41646beadabe35d903523359e22ecb8781e
rafaelholland <rafael.holland@bandtec.com.br>
2021-10-07 18:26:29 -0300

    added new routes

--- Back-End/src/main/java/br/com/queerfy/backend/repositories/PropertyRepository.java
+++ Back-End/src/main/java/br/com/queerfy/backend/repositories/PropertyRepository.java
@@ -3,8 +3,14 @@ package br.com.queerfy.backend.repositories;
 import br.com.queerfy.backend.entities.Property;
 import br.com.queerfy.backend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Integer> {
+
+    @Query(value = "select p from Property p where p.active = true")
+    List<Property> findAllAvaliableProperties();
 }
--- Back-End/src/main/java/br/com/queerfy/backend/services/PropertyService.java
+++ Back-End/src/main/java/br/com/queerfy/backend/services/PropertyService.java
@@ -7,12 +7,16 @@ import br.com.queerfy.backend.exceptions.UserNotFoundException;
 import br.com.queerfy.backend.repositories.AddressesRepository;
 import br.com.queerfy.backend.repositories.PropertyRepository;
 import br.com.queerfy.backend.repositories.UserRepository;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PropertyService {
@@ -37,5 +41,41 @@ public class PropertyService {
         throw new UserNotFoundException();
     }
 
+    @Transactional
+    public List<PropertyDTO> getAllProperties(){
+        List<PropertyDTO> list = propertyRepository.findAll().stream()
+                .map(property -> new PropertyDTO(property)).collect(Collectors.toList());
+        return list;
+    }
+    @Transactional
+    public PropertyDTO getPropertyById(Integer id) throws UserNotFoundException {
+        try{
+            Property property = propertyRepository.findById(id).get();
+            return new PropertyDTO(property);
+        }catch(EntityNotFoundException e){
+            throw new UserNotFoundException();
+        }
+    }
+    @Transactional
+    public PropertyDTO updateProperty(Integer id, Property property) throws UserNotFoundException {
+        Optional<Property> entity = propertyRepository.findById(id);
+        if (entity.isPresent()){
+            Property prop = entity.get();
+            prop.setUser(property.getUser());
+            prop.setActive(property.getActive());
+            prop.setCheckIn(property.getCheckIn());
+            prop.setCheckOut(property.getCheckOut());
+            prop.setDailyPrice(property.getDailyPrice());
+            prop.setFilterDate(property.getFilterDate());
+            prop.setHouseImg(property.getHouseImg());
+            prop.setLatitude(property.getLatitude());
+            prop.setLongitude(property.getLongitude());
+
+            propertyRepository.save(prop);
+
+            return new PropertyDTO(prop);
+        }
+        throw new UserNotFoundException();
+    }
 
 }
