05e83d24a88797462588d5434bf20cc45eae67db
IgorSilvaZZ <igor06silva12@gmail.com>
2021-11-01 14:51:24 -0300

    Add initial function

--- Front-end/src/components/Map/index.tsx
+++ Front-end/src/components/Map/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { NextPage } from 'next';
 
+import L from "leaflet";
+
 import { MapContainer as LeafletMap, TileLayer, MapContainerProps as LeafletMapProps, Marker, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 
-import L from "leaflet";
-
-/* import mapMarkerICon from "../../../public/markerMap.png"; */
+import { MapContainer, PopupStyled } from "./style";
 
 interface MapProps extends LeafletMapProps {
   interactive?: boolean;
@@ -14,29 +14,50 @@ interface MapProps extends LeafletMapProps {
 }
 
 const markerIconMap = L.icon({
-  iconUrl: "../../../public/markerMap.png",
+  iconUrl: "map-pin.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
-})
+});
 
 const Map: NextPage = ({ children, interactive = true, ...props }: MapProps) => {
 
   const [ initialPosition, setInitialPosition ] = useState<[number, number]>([0, 0]);
 
-  useEffect(() => {
+  /* const getLocations = () => {
+    let latitude;
+    let longitude;
     if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        console.log("latitude: ", position.coords.latitude);
-        console.log("longitude: ", position.coords.longitude);
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        setInitialPosition([latitude, longitude]);
+        navigator.geolocation.getCurrentPosition((position) => {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
       })
     }
+    setInitialPosition([latitude, longitude]);
+  } */
+
+  const getLatitude = new Promise<Number|null>((resolve, reject): void => {
+    let latitude: Number;
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        latitude = position.coords.latitude;
+        resolve(Number(latitude));
+      })
+    }else {
+      reject(null);
+    }
+  })
+
+  const getLocations = async() => {
+    const latitude = await getLatitude();
+    console.log(latitude);
+  }
+
+  useEffect(() => {
+    /* getLocations(); */
   }, []);
 
   return(
-    <div style={{ border: '2px solid blue', height: '500px', marginBottom: '20px' }}>
+    <MapContainer>
       <LeafletMap
         style={{ width: '100%', height: '100%' }}
         center={[-23.5582664, -46.66144]}
@@ -47,13 +68,15 @@ const Map: NextPage = ({ children, interactive = true, ...props }: MapProps) => 
           /* url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" */
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`}
         />
-        <Marker position={[-23.5582664, -46.66144]}>
-          <Popup>
-            A pretty CSS3 popup, <br /> Easily customizable
-          </Popup>
+        <Marker position={[-23.5582664, -46.66144]} icon={markerIconMap}>
+          <PopupStyled>
+            <a href="">
+              Informações da casa <br /> vai estar aqui.
+            </a>
+          </PopupStyled>
         </Marker>
       </LeafletMap>
-    </div>
+    </MapContainer>
   )
 }
 
