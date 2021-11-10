ad30d949289c2bbd49c1192c4fc82959e3fc47f5
IgorSilvaZZ <igor06silva12@gmail.com>
2021-11-10 00:50:16 -0300

    Ajust styles in page

--- Front-end/src/pages/Chat/index.tsx
+++ Front-end/src/pages/Chat/index.tsx
@@ -2,27 +2,47 @@ import { NextPage } from 'next';
 import io from 'socket.io-client';
 
 import {
-  BodyContainer,
   Main,
-  ChatContainer,
+  MainContainer,
+  TitleChat,
   ContainerChat,
-  MessageUserLoged,
-  MessageContainer,
+  MessageUser,
+  ContainerMessage,
+  UsernameLoged,
+  MessageBox,
+  DateMessage,
+  FooterChat,
+  FooterInput,
+  ButtonSendMessage,
 } from './style';
 
 const Chat: NextPage = () => {
   return (
-    <BodyContainer>
-      <Main>
-        <ChatContainer>
-          <ContainerChat>
-            <MessageUserLoged>
-              <MessageContainer></MessageContainer>
-            </MessageUserLoged>
-          </ContainerChat>
-        </ChatContainer>
-      </Main>
-    </BodyContainer>
+    <Main>
+      <MainContainer>
+        <TitleChat>Mensagens</TitleChat>
+        <ContainerChat>
+          <MessageUser userLoged>
+            <ContainerMessage userLoged>
+              <UsernameLoged userLoged>Igor</UsernameLoged>
+              <MessageBox>Oi, tudo bem?</MessageBox>
+              <DateMessage>10/11/2021</DateMessage>
+            </ContainerMessage>
+          </MessageUser>
+          <MessageUser>
+            <ContainerMessage>
+              <UsernameLoged>Lemos</UsernameLoged>
+              <MessageBox>Oi, tudo sim e com você?</MessageBox>
+              <DateMessage>10/11/2021</DateMessage>
+            </ContainerMessage>
+          </MessageUser>
+        </ContainerChat>
+        <FooterChat>
+          <FooterInput></FooterInput>
+          <ButtonSendMessage></ButtonSendMessage>
+        </FooterChat>
+      </MainContainer>
+    </Main>
   );
 };
 
--- Front-end/src/pages/Chat/style.ts
+++ Front-end/src/pages/Chat/style.ts
@@ -1,66 +1,88 @@
 import styled from 'styled-components';
 
-export const BodyContainer = styled.body`
+interface UserLogedMessage {
+  userLoged?: true;
+}
+
+export const Main = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
+
   height: 100vh;
-  background: #353b48;
+  width: 100vw;
   overflow-y: hidden;
 
-  * ::-webkit-scrollbar {
-    width: 8px;
-    background: #171717;
-  }
-  * ::-webkit-scrollbar-thumb {
-    background: #5f27cd;
-    border-radius: 5px;
-  }
+  background: #353b48;
 `;
 
-export const Main = styled.div`
-  display: flex;
-  height: 100%;
-  width: 90%;
-  border-radius: 0px 20px 20px 0px;
-`;
-
-export const ChatContainer = styled.div`
+export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 75%;
-  height: 100%;
+
+  height: 90%;
+  width: 90%;
+
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: 20px;
+`;
+
+export const TitleChat = styled.p`
+  text-align: center;
+  font-size: 25px;
+  font-weight: 500;
+
+  padding: 15px;
+
+  color: white;
+  border-bottom: 1px solid #636e72;
 `;
 
 export const ContainerChat = styled.div`
   display: flex;
   flex-direction: column;
+
   width: 100%;
   height: 75%;
+
   overflow-y: scroll;
   overflow-y: auto;
   overflow-x: hidden;
 `;
 
-export const MessageUserLoged = styled.div`
+export const MessageUser = styled.div<UserLogedMessage>`
   display: flex;
-  justify-content: flex-end;
+  justify-content: ${(props) => (props.userLoged ? 'flex-end' : 'flex-start')};
 `;
 
-export const MessageOtherUser = styled.div`
-  display: flex;
-  justify-content: flex-start;
-`;
-
-export const MessageContainer = styled.div`
+export const ContainerMessage = styled.div<UserLogedMessage>`
   display: flex;
   flex-direction: column;
+
   width: 300px;
+
+  background: #3c096c;
+
   margin-top: 1%;
   margin-bottom: 3px;
   margin-right: 3%;
+
   padding: 10px;
   margin: 10px;
+
+  border-radius: ${(props) =>
+    props.userLoged ? '10px 10px 0px 10px' : '10px 10px 10px 0px'};
+`;
+
+export const UsernameLoged = styled.span<UserLogedMessage>`
+  display: flex;
+  justify-content: end;
+
+  color: ${(props) => (props.userLoged ? '#240046' : 'purple')};
+  font-size: 10px;
+
+  font-weight: bold;
+  margin-bottom: 8px;
 `;
 
 export const MessageBox = styled.span`
@@ -68,29 +90,13 @@ export const MessageBox = styled.span`
   font-size: 10px;
 `;
 
-export const UsernameLoged = styled.span`
-  color: #240046;
-  display: flex;
-  justify-content: end;
-  font-size: 10px;
-  font-weight: bold;
-  margin-bottom: 8px;
-`;
-
-export const OtherUsername = styled.span`
-  color: purple;
-  display: flex;
-  justify-content: end;
-  font-size: 10px;
-  font-weight: bold;
-  margin-bottom: 8px;
-`;
-
 export const DateMessage = styled.span`
-  color: white;
-  font-size: 10px;
   display: flex;
   justify-content: flex-end;
+
+  color: white;
+
+  font-size: 10px;
   margin-top: 3px;
 `;
 
@@ -98,33 +104,35 @@ export const FooterChat = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+
   width: 100%;
   height: 20%;
 `;
 
-export const FooterChatInput = styled.input`
-  height: 60%;
-  width: 75%;
+export const FooterInput = styled.input`
+  height: 55%;
+  width: 80%;
+
   font-size: 15px;
   background: rgba(73, 80, 87, 0.5);
+
   border: none;
   outline: none;
+
   color: #636e72;
   border-radius: 10px;
   padding: 0px 8px;
 `;
 
-export const buttonSendMessage = styled.div`
-  height: 60%;
-  width: 12%;
+export const ButtonSendMessage = styled.div`
+  height: 55%;
+  width: 8%;
+
   border-radius: 10px;
   background: #5f27cd;
+
   transition: background-color 0.3s;
   cursor: pointer;
-  /* background-image: url('../images/email.svg');
-    background-size: 50%;
-    background-repeat: no-repeat;
-    background-position: center; */
 
   &:hover {
     background-color: #341f97;
