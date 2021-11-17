0597700749c9de2a82bb0a85c9dd5e027f612363
IgorSilvaZZ <igor06silva12@gmail.com>
2021-11-16 21:18:44 -0300

    Add types slideshow!

--- Front-end/src/pages/Chat/index.tsx
+++ Front-end/src/pages/Chat/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef } from 'react';
 import { NextPage } from 'next';
 import dayjs from 'dayjs';
 import io from 'socket.io-client';
+import { Slide } from 'react-slideshow-image';
 
 import { useAuth } from '../../hooks/useAuth';
 
