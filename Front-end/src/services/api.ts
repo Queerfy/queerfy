9d8655b0a708f06dd458b5f382fc5677e85b7036
IgorSilvaZZ <igor06silva12@gmail.com>
2022-06-01 11:29:28 -0300

    Ajust api url

--- Front-end/src/services/api.ts
+++ Front-end/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://18.206.113.23:8080',
+  baseURL: 'queerfybackend.ddns.net:8080/',
 });
 
 export const apiGeocode = axios.create({
